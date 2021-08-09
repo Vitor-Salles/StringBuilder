9832c51e5295102d173f4ac665cd78df9b2d8a31
Vitor Salles <vitorsalles.santos@hotmail.com>
2021-08-09 12:33:59 -0300

    Iniciado exercicio com StringBuilder

---
+++ Exercicio - S9_122 (StringBuilder)/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio___S9_122__StringBuilder_.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
